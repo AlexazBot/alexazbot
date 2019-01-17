1149f4e64ae3ec43fa1759e92ed7f25d01e248eb
AlexazBot <46775932+AlexazBot@users.noreply.github.com>
2019-01-17 15:00:09 +0100

    Add files via upload

---
+++ comand.php
@@ -0,0 +1,9 @@
+<?php
+  $comandi = array(
+    "ciao" => "Ciao!",
+    "chi sei" => "Sono Alexa",
+    "come stai" => "Molto bene grazie",
+    "che ore sono" => date("H:i"),
+    "che giorno è" => date("d/m/y")
+  );
+?>

4d230f6b58753efbcfd69b1458ccb3f9d7e11ac1
AlexazBot <46775932+AlexazBot@users.noreply.github.com>
2019-01-17 09:36:59 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://supsupersupbot.herokuapp.com/execute.php';
-$BOT_TOKEN = '679382450:AAGPBg99bYECXBCPtq0aXBetw_9uFUczJro';
+$WEBHOOK_URL = 'https://alexazbot.herokuapp.com/execute.php';
+$BOT_TOKEN = '695677148:AAEY4NoqyJ67j4YwARWvXabMOhj76yKWaoM';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';

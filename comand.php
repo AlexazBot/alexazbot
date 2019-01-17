f054c3ac2ccce6aafcad950e1a9c93f31bece9e9
AlexazBot <46775932+AlexazBot@users.noreply.github.com>
2019-01-17 14:57:59 +0100

    Delete comand.php

--- comand.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  $comand = array(
-    'ciao' => "Ciao!",
-    'chi sei' => "Sono Alexa",
-    'come stai' => "Molto bene grazie",
-    'che ore sono' => date("H:i"),
-    'che giorno è' => date("d/m/y")
-  );
-?>

1a62861a2e509b1490cfe14658af71c63931b776
AlexazBot <46775932+AlexazBot@users.noreply.github.com>
2019-01-18 11:57:57 +0100

    Update command.php

--- command.php
+++ command.php
@@ -5,7 +5,7 @@
     "hey" => "Hey!",
     "chi sei" => "Sono Alexaz!",
     "come stai" => "Molto bene grazie",
-    "* ore" => "Sono le ore " . date("H:i"),
+    "ore" => "Sono le ore " . date("H:i"),
     "che giorno è" => "Oggi è il giorno " . date("d/m/y")
   );
 ?>
